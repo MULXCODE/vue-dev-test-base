0b5160964c1ad7bb8eed60afa5f3a6316baa6793
MULX <mulxman@gmail.com>
2023-08-29 19:02:37 +1000

    BreedsList Component base added to the BreedsView
    also fixed too much linting slowing down builds

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    <router-link to="/breeds">Breeds</router-link>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
@@ -23,7 +24,7 @@ nav {
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #426cb9;
     }
   }
 }
--- src/views/BreedsView.vue
+++ src/views/BreedsView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="breeds">
     <h1>Breeds</h1>
+    <!-- <img alt="Logo" src="../assets/logo.png" /> -->
+    <BreedsList />
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import BreedsList from "@/components/BreedsList.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    BreedsList,
+  },
+})
+export default class BreedsView extends Vue {}
+</script>
